d3f2951d3996617c34a0c1c4a52c76dc13e7e09a
Danny Willett <dannywill8@gmail.com>
2021-04-26 01:23:58 -0400

    Add project files.

---
+++ UncreatedWarfare/Config.cs
@@ -0,0 +1,39 @@
+﻿using Rocket.API;
+using System.Xml.Serialization;
+
+namespace UncreatedWarfare
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        [XmlElement("Modules")]
+        public Modules Modules;
+        public void LoadDefaults()
+        {
+            Modules = new Modules();
+        }
+    }
+    public class Modules
+    {
+        public bool PlayerList;
+        public bool Kits;
+        public bool Rename;
+        public bool VehicleSpawning;
+        public bool FOBs;
+        public bool Roles;
+        public bool UI;
+        public bool AdminLogging;
+        public bool MainCampPrevention;
+        public Modules()
+        {
+            this.PlayerList = true;
+            this.Kits = true;
+            this.Rename = true;
+            this.VehicleSpawning = true;
+            this.FOBs = true;
+            this.Roles = true;
+            this.UI = true;
+            this.AdminLogging = true;
+            this.MainCampPrevention = true;
+        }
+    }
+}
---
+++ UncreatedWarfare/UCWarfare.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Rocket.Core.Logging.Logger;
+
+namespace UncreatedWarfare
+{
+    public class UCWarfare : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            base.Load();
+        }
+        protected override void Unload()
+        {
+            base.Unload();
+        }
+    }
+}
