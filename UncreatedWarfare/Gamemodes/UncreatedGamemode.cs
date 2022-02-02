f55064d4be0bf0d8c7e154434b773ab1047ea2e7
Danny Willett <dannywill8@gmail.com>
2022-02-01 19:45:51 -0500

    pull

--- UncreatedWarfare/Gamemodes/UncreatedGamemode.cs
+++ UncreatedWarfare/Gamemodes/UncreatedGamemode.cs
@@ -160,17 +160,21 @@ namespace Uncreated.Warfare.Gamemodes
                 Gamemode gamemode = UCWarfare.I.gameObject.AddComponent(nextMode) as Gamemode;
                 if (gamemode != null)
                 {
+                    Data.Gamemode = gamemode;
                     gamemode.Init();
                     gamemode.OnLevelLoaded();
                     //Chat.Broadcast("force_loaded_gamemode", Data.Gamemode.DisplayName);
                     for (int i = 0; i < Provider.clients.Count; i++)
                         gamemode.OnPlayerJoined(UCPlayer.FromSteamPlayer(Provider.clients[i]), true, true);
                     L.Log("Chosen new gameode " + gamemode.DisplayName, ConsoleColor.DarkCyan);
-                    Data.Gamemode = gamemode;
                     _state = EState.DISCARD;
                     Destroy(this);
                     return false;
                 }
+            }
+            for (int i = 0; i < Provider.clients.Count; i++)
+            {
+                Data.Gamemode.OnPlayerJoined(UCPlayer.FromSteamPlayer(Provider.clients[i]), true, false);
             }
             return true;
         }
