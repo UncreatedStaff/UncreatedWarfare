efb76f83eea3960d68c9f20108bbf55c5c22c472
Danny Willett <dannywill8@gmail.com>
2023-01-22 12:57:54 -0500

    idk whats in this

--- UncreatedWarfare/Configuration/JSONMethods.cs
+++ UncreatedWarfare/Configuration/JSONMethods.cs
@@ -13,60 +13,6 @@ using UnityEngine;
 
 namespace Uncreated.Warfare;
 
-public readonly struct TranslationData
-{
-    public static readonly TranslationData Nil = new TranslationData("default", "<color=#ffffff>default</color>", true, Color.white);
-    public readonly string Message;
-    public readonly string Original;
-    public readonly Color Color;
-    public readonly bool UseColor;
-    public TranslationData(string message, string original, bool useColor, Color color)
-    {
-        this.Message = message;
-        this.Original = original;
-        this.UseColor = useColor;
-        this.Color = color;
-    }
-    public TranslationData(string Original)
-    {
-        this.Original = Original;
-        this.Color = GetColorFromMessage(Original, out Message, out UseColor);
-    }
-    public static TranslationData GetPlaceholder(string key) => new TranslationData(key, key, false, Color.white);
-    public static Color GetColorFromMessage(string Original, out string InnerText, out bool found)
-    {
-        if (Original.Length < 23)
-        {
-            InnerText = Original;
-            found = false;
-            return UCWarfare.GetColor("default");
-        }
-        if (Original.StartsWith("<color=#") && Original[8] != '{' && Original.EndsWith("</color>"))
-        {
-            IEnumerator<char> characters = Original.Skip(8).GetEnumerator();
-            int start = 8;
-            int length = 0;
-            while (characters.MoveNext())
-            {
-                if (characters.Current == '>') break; // keep moving until the ending > is found.
-                length++;
-            }
-            characters.Dispose();
-            int msgStart = start + length + 1;
-            InnerText = Original.Substring(msgStart, Original.Length - msgStart - 8);
-            found = true;
-            return Original.Substring(start, length).Hex();
-        }
-        else
-        {
-            InnerText = Original;
-            found = false;
-            return UCWarfare.GetColor("default");
-        }
-    }
-    public readonly override string ToString() =>
-        $"Original: {Original}, Inner text: {Message}, {(UseColor ? $"Color: {Color} ({ColorUtility.ToHtmlStringRGBA(Color)}." : "Unable to find color.")}";
-}
 public struct Point3D
 {
     public string name;
