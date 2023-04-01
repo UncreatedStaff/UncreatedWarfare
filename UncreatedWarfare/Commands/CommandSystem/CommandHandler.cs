7c7f8276d4e86eda5086e4fb7ad7d309ad7ccfa0
Danny Willett <dannywill8@gmail.com>
2023-04-01 12:44:56 -0400

    Added timeout to command waiter.

--- UncreatedWarfare/Commands/CommandSystem/CommandHandler.cs
+++ UncreatedWarfare/Commands/CommandSystem/CommandHandler.cs
@@ -24,7 +24,7 @@ public static class CommandHandler
     private static readonly ArgumentInfo[] ArgBuffer = new ArgumentInfo[MaxArgCount];
     internal static CancellationTokenSource GlobalCommandCancel = new CancellationTokenSource();
     internal static List<CommandInteraction> ActiveCommands = new List<CommandInteraction>(8);
-    internal static bool TryingToCancel;
+    internal static volatile bool TryingToCancel;
     private static readonly List<PendingChatMessage> PendingMessages = new List<PendingChatMessage>(32);
     static CommandHandler()
     {
@@ -34,36 +34,43 @@ public static class CommandHandler
     }
     public static async Task LetCommandsFinish()
     {
-        TryingToCancel = true;
-        GlobalCommandCancel.CancelAfter(CommandMaxTimeoutAtGameEndMs);
-        foreach (CommandInteraction intx in ActiveCommands.ToList())
+        try
         {
-            if (intx.Task != null && !intx.Task.IsCompleted)
+            TryingToCancel = true;
+            GlobalCommandCancel.CancelAfter(CommandMaxTimeoutAtGameEndMs);
+            foreach (CommandInteraction intx in ActiveCommands.ToList())
             {
-                try
+                if (intx.Task != null && !intx.Task.IsCompleted)
                 {
-                    await intx.Task.ConfigureAwait(false);
+                    try
+                    {
+                        await Task.Run(() => Task.WaitAny(new Task[] { intx.Task }, 3000), new CancellationTokenSource(3000).Token);
+                    }
+                    catch (OperationCanceledException) { }
                 }
-                catch (OperationCanceledException) { }
             }
-        }
 
-        GlobalCommandCancel = new CancellationTokenSource();
-        await UCWarfare.ToUpdate(GlobalCommandCancel.Token);
-        TryingToCancel = false;
-        for (int i = 0; i < PendingMessages.Count; i++)
-        {
-            PendingChatMessage msg = PendingMessages[i];
-            if (msg.Player == null || msg.Player.IsOnline)
+            GlobalCommandCancel = new CancellationTokenSource();
+            await UCWarfare.ToUpdate(GlobalCommandCancel.Token);
+            TryingToCancel = false;
+            for (int i = 0; i < PendingMessages.Count; i++)
             {
-                bool shouldList = true;
-                CheckRunCommand(msg.Player, msg.Message, ref shouldList, msg.RequirePrefix);
-                if (shouldList && msg.Player is { IsOnline: true } pl)
-                    ChatManager.serverSendMessage(msg.Message, Palette.AMBIENT, pl.SteamPlayer, null, msg.ChatMode);
+                PendingChatMessage msg = PendingMessages[i];
+                if (msg.Player == null || msg.Player.IsOnline)
+                {
+                    bool shouldList = true;
+                    CheckRunCommand(msg.Player, msg.Message, ref shouldList, msg.RequirePrefix);
+                    if (shouldList && msg.Player is { IsOnline: true } pl)
+                        ChatManager.serverSendMessage(msg.Message, Palette.AMBIENT, pl.SteamPlayer, null, msg.ChatMode);
+                }
             }
-        }
 
-        PendingMessages.Clear();
+            PendingMessages.Clear();
+        }
+        catch (Exception ex)
+        {
+            L.LogError(ex);
+        }
     }
 
     private readonly struct PendingChatMessage
