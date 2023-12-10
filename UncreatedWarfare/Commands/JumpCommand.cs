ae74d987d39e9d1863c8a4cb69df5a4c33c6ef74
Daniel Willett <12886600+DanielWillett@users.noreply.github.com>
2023-12-10 02:12:46 -0500

    Update JumpCommand.cs

--- UncreatedWarfare/Commands/JumpCommand.cs
+++ UncreatedWarfare/Commands/JumpCommand.cs
@@ -9,26 +9,35 @@ public class JumpCommand : Command
     private const string Syntax = "/jump [player] <multiplier>";
     private const string Help = "Sets a player's jump modifier.";
 
-    public JumpCommand() : base("speed", EAdminType.MODERATOR){
-        Structure = new CommandStructure {
+    public JumpCommand() : base("jump", EAdminType.MODERATOR)
+    {
+        Structure = new CommandStructure
+        {
             Description = Help,
-            Parameters = new CommandParameter[] {
-                new CommandParameter("player", typeof(IPlayer)) {
+            Parameters = new CommandParameter[]
+            {
+                new CommandParameter("player", typeof(IPlayer))
+                {
                     Description = "The player whose jump multiplier changes. Omit to target yourself.",
-                    Parameters = new CommandParameter[] {
-                        new CommandParameter("mulitplier", typeof(float)) {
+                    Parameters = new CommandParameter[]
+                    {
+                        new CommandParameter("mulitplier", typeof(float))
+                        {
                             Description = "Change the jump multiplier of the target player."
                         },
-                        new CommandParameter("default") {
+                        new CommandParameter("default")
+                        {
                             Aliases = new string[] { "reset" },
                             Description = "Set the jump multiplier to 1x."
                         }
                     }
                 },
-                new CommandParameter("mulitplier", typeof(float)) {
+                new CommandParameter("mulitplier", typeof(float))
+                {
                     Description = "Change the jump multiplier of the target player."
                 },
-                new CommandParameter("default") {
+                new CommandParameter("default")
+                {
                     Aliases = new string[] { "reset" },
                     Description = "Set the jump multiplier to 1x."
                 }
@@ -36,7 +45,8 @@ public class JumpCommand : Command
         };
     }
 
-    public override void Execute(CommandInteraction ctx) {
+    public override void Execute(CommandInteraction ctx)
+    {
         ctx.AssertArgs(1, Syntax);
         ctx.AssertHelpCheck(0, Syntax + " - " + Help);
 
@@ -51,7 +61,8 @@ public class JumpCommand : Command
             throw ctx.SendPlayerOnlyError();
 
         int multParamIndex = ctx.HasArgs(2) ? 1 : 0;
-        if (!ctx.TryGet(multParamIndex, out float multiplier)) {
+        if (!ctx.TryGet(multParamIndex, out float multiplier))
+        {
             if (!ctx.MatchParameter(multParamIndex, "reset", "default"))
                 throw ctx.Reply(T.JumpMultiplierInvalidValue, ctx.Get(multParamIndex)!);
 
