990694d286c05233113a8239f27ea8fb64edcad6
420 <benvalkin@outlook.com>
2024-10-25 23:20:25 +0200

    Added more FOB events

---
+++ UncreatedWarfare/Events/Models/Fobs/FobDestroyed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uncreated.Warfare.Fobs;
+using Uncreated.Warfare.FOBs;
+using Uncreated.Warfare.Players;
+
+namespace Uncreated.Warfare.Events.Models.Fobs;
+
+/// <summary>
+/// Event listener args which fires after the main structure of any <see cref="BasePlayableFob"/> subclass is destroyed.
+/// </summary>
+internal class FobDestroyed
+{
+    /// <summary>
+    /// The <see cref="BasePlayableFob"/> that was destroyed.
+    /// </summary>
+    public required BasePlayableFob Fob { get; init; }
+}
---
+++ UncreatedWarfare/Events/Models/Fobs/FobSuppliesChanged.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uncreated.Warfare.Fobs;
+using Uncreated.Warfare.FOBs;
+using Uncreated.Warfare.FOBs.SupplyCrates;
+using Uncreated.Warfare.Players;
+
+namespace Uncreated.Warfare.Events.Models.Fobs;
+
+/// <summary>
+/// Event listener args which fires after supplies are added or removed from a <see cref="IResourceFob"/>.
+/// </summary>
+internal class FobSuppliesChanged
+{
+    /// <summary>
+    /// The <see cref="IResourceFob"/> where supplies were added or removed.
+    /// </summary>
+    public required IResourceFob Fob { get; init; }
+    /// <summary>
+    /// The number of supplies that were added (positive) or removed (negative).
+    /// </summary>
+    public required int AmountDelta { get; init; }
+    /// <summary>
+    /// The type of supplies that were added or removed.
+    /// </summary>
+    public required SupplyType SupplyType { get; init; }
+    /// <summary>
+    /// The reason the supplies were added or removed.
+    /// </summary>
+    public required SupplyChangeReason ChangeReason { get; init; }
+}
