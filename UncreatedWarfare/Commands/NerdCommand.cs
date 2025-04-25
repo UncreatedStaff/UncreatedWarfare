77dd4bb4695f98edf877c2576f07a5ad26fb137d
AngelAbov3 <JoshMcGeeNZ@outlook.com>
2025-04-25 16:01:23 +1200

    Tried to Add Nerd Command

---
+++ UncreatedWarfare/Commands/NerdCommand.cs
@@ -0,0 +1,31 @@
+﻿using Uncreated.Warfare.Interaction.Commands;
+using Uncreated.Warfare.Interaction;
+
+namespace Uncreated.Warfare.Commands;
+
+[Command("nerd"), MetadataFile]
+internal sealed class NerdCommand : IExecutableCommand
+{
+    private readonly NerdService _nerdService;
+
+    /// <inheritdoc />
+    public required CommandContext Context { get; init; }
+
+    public NerdCommand(NerdService nerdService)
+    {
+        _nerdService = nerdService;
+    }
+
+    /// <inheritdoc />
+    public async UniTask ExecuteAsync(CancellationToken token)
+    {
+        Context.AssertRanByPlayer();
+        /*
+                if (!await _nerdService.SetNerdnessAsync(token).ConfigureAwait(false))
+                {
+                    throw Context.SendNoPermission();
+                }
+        */
+        Context.Defer();
+    }
+}
