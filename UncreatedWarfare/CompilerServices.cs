cd6c880ad088f91280471b661d72708734f2798d
DanielWillett <dannywill8@gmail.com>
2025-06-06 00:21:03 -0400

    Fixed SetsRequiredMembersAttribute.

--- UncreatedWarfare/CompilerServices.cs
+++ UncreatedWarfare/CompilerServices.cs
@@ -1,32 +1,36 @@
 // ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices;
-
-sealed class IsExternalInit;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
-sealed class RequiredMemberAttribute : Attribute;
-
-[AttributeUsage(AttributeTargets.Constructor)]
-sealed class SetsRequiredMembersAttribute : Attribute;
-
-[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
-sealed class CompilerFeatureRequiredAttribute(string featureName) : Attribute
+namespace System.Runtime.CompilerServices
 {
-    public const string RefStructs = "RefStructs";
-    public const string RequiredMembers = "RequiredMembers";
-    public string FeatureName { get; } = featureName;
-    public bool IsOptional { get; init; }
+    sealed class IsExternalInit;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    sealed class RequiredMemberAttribute : Attribute;
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    sealed class CompilerFeatureRequiredAttribute(string featureName) : Attribute
+    {
+        public const string RefStructs = "RefStructs";
+        public const string RequiredMembers = "RequiredMembers";
+        public string FeatureName { get; } = featureName;
+        public bool IsOptional { get; init; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+    sealed class InterpolatedStringHandlerAttribute : Attribute;
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    sealed class InterpolatedStringHandlerArgumentAttribute : Attribute
+    {
+        public InterpolatedStringHandlerArgumentAttribute(string argument) => Arguments = [argument];
+
+        public InterpolatedStringHandlerArgumentAttribute(params string[] arguments) => Arguments = arguments;
+
+        public string[] Arguments { get; }
+    }
 }
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
-sealed class InterpolatedStringHandlerAttribute : Attribute;
-
-[AttributeUsage(AttributeTargets.Parameter)]
-sealed class InterpolatedStringHandlerArgumentAttribute : Attribute
+namespace System.Diagnostics.CodeAnalysis
 {
-    public InterpolatedStringHandlerArgumentAttribute(string argument) => Arguments = [ argument ];
-
-    public InterpolatedStringHandlerArgumentAttribute(params string[] arguments) => Arguments = arguments;
-
-    public string[] Arguments { get; }
+    [AttributeUsage(AttributeTargets.Constructor)]
+    sealed class SetsRequiredMembersAttribute : Attribute;
 }
